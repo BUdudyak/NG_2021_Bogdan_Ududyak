386d95953d2e9f03139c7c00fcff0b46c939a4a1
Bogdan Ududyak <uududyak@gmail.com>
2021-10-13 17:01:44 +0300

    In this task i have done a little picture with dota and enigma:)

---
+++ Lesson 1/Task_2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "   .---------. " << endl;
+    cout << "   |.-------.| " << endl;
+    cout << "   ||>dota# || " << endl;
+    cout << "   ||       || " << endl;
+    cout << "   |'-------'|etf " << endl;
+    cout << " .-^---------^-. " << endl;
+    cout << " | ---~  Enigma| " << endl;
+    cout << " '-------------' " << endl;
+
+    return 0;
+}
