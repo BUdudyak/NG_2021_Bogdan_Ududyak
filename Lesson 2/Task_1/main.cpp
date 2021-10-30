4449d35e76cd5c8364b879bc38b7514e861a023b
Bogdan Ududyak <uududyak@gmail.com>
2021-10-31 02:15:41 +0300

    Added Lesson 2/Task_1/main.cpp where u can find out sum of digitals

---
+++ Lesson 2/Task_1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter your number: ";
+    int  number;
+    int sum;
+    cin >> number;
+    for(int a = number; a != 0; a2 /= 10 ){
+        sum += a % 10;
+    }
+    cout << "Sum of your numers is: " << sum;
+    return 0;
+}
