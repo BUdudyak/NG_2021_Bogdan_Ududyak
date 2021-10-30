c1b58dadb007a6f9b3bb2a8f1548faf892d230e5
Bogdan Ududyak <uududyak@gmail.com>
2021-10-30 23:46:05 +0300

    Update Lesson 1/Task_5/main.cpp

--- Lesson 1/Task_5/main.cpp
+++ Lesson 1/Task_5/main.cpp
@@ -5,13 +5,9 @@ using namespace std;
 int main()
 {
     int fuel = 0;
-    int yearlights;
     cout << "Enter starship weight in tones - ";
     cin >> fuel;
-    fuel = fuel/3-2;
-    cout << "About year-lights to travel - ";
-    cin >> yearlights;
-    fuel = fuel * yearlights;
+    fuel = (fuel/3-2)*300;
     cout << "You need about " << fuel << " fuel in tones to travel 300 year-lights";
     return 0;
 }
