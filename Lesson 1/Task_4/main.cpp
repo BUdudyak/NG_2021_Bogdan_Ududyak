c56a0f9f65d95d7494e646161fcf602b925eb080
Bogdan Ududyak <uududyak@gmail.com>
2021-11-14 03:08:37 +0200

    Modifided code stile

--- Lesson 1/Task_4/main.cpp
+++ Lesson 1/Task_4/main.cpp
@@ -18,20 +18,20 @@ int main()
     cin >> c;
     D = b*b - 4*a*(-c);
     if  (D < 0)
-        {
+    {
         cout << "there are no roots";
-        }
+    }
     if (D > 0)
-        {
+    {
         x1 = (-b + sqrt(D))/2*(-1);
         x2 = (-b - sqrt(D))/2*(-1);
         cout << "x1 = " << x1 << endl;
         cout << "x2 = " << x2;
-        }
+    }
     if  (D = 0)
-        {
+    {
         x1 = (-b +sqrt(D))/2*(-1);
         cout << "x = " << x1;
-        }
+    }
     return 0;
 }
