6807e87b20b60ce7fb3ae5f581d49bad53c4dd53
Bogdan Ududyak <uududyak@gmail.com>
2021-11-14 03:43:57 +0200

    Counts how many characters you have entered

---
+++ Lesson 4/Task_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int num;
+    string ch;
+    cin >> ch;
+    num = ch.size();
+    cout << num;
+    return 0;
+}
