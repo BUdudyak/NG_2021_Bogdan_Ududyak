787fa12c95ee5c6fc29a4e6dafd667b8e07a9a2a
Bogdan Ududyak <uududyak@gmail.com>
2021-12-08 13:18:56 +0200

    Refreshed code and changed variables

--- Lesson 3/Task_1/main.cpp
+++ Lesson 3/Task_1/main.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main()
 {
-    int number = 0, dij = 0, num,  chi = 0, a[10] = {0};
-    for(number = 0; number < 10; number++)
+    int dijital = 0, numb,  chislo = 0, array[10] = {0};
+    for(int number = 0; number < 10; number++)
     {
-        cout << "a[" << dij << "] = ";
-        cin >> chi;
-        a[number] = chi;
-        dij++;
+        cout << "array[" << dijital << "] = ";
+        cin >> chislo;
+        array[number] = chislo;
+        dijital++;
     }
     cout << "Enter your number - ";
-    cin >> num;
-        for(int number : a)
+    cin >> numb;
+        for(int number : array)
         {
-            cout << number + num << " ";
+            cout << number + numb << " ";
         }
     return 0;
 }
